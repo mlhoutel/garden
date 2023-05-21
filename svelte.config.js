8aae74e0c0ead71d9c2f0b2da867fc957e4b0cab
mlhoutel <mlhoutel@gmail.com>
2023-05-21 15:11:32 +0200

    build: :rocket: Configure deployment for Vercel build and hosting
    Use the auto-adapter and remove the path patch

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import preprocess from 'svelte-preprocess';
 import importAssets from 'svelte-preprocess-import-assets';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
+/*
+// STATIC DEPLOYMENT
+// if you want to deploy to gtihub pages, uncomment this block
+// as well as the "paths" property in the code below.
+
+import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
+const base = dev ? '' : '/garden'
+*/
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,9 +20,7 @@ const config = {
 	preprocess: [mdsvex(mdsvexConfig), preprocess(), importAssets()],
 	kit: {
 		adapter: adapter({ strict: false }),
-		paths: {
-			base: dev ? '' : '/garden'
-		}
+		// paths: { base: base }
 	}
 };
 
